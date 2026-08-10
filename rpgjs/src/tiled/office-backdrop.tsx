7b0cd30d6188777b0907fe34d02573e1aa644480
Steffen Retzlaff <s.retzlaff@web.de>
2026-08-10 08:13:36 +0800

    Add backdrop tileset

---
+++ rpgjs/src/tiled/office-backdrop.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="office-backdrop" tilewidth="768" tileheight="480" tilecount="1" columns="0">
+ <tile id="0">
+  <image source="office-backdrop.svg" width="768" height="480"/>
+ </tile>
+</tileset>
